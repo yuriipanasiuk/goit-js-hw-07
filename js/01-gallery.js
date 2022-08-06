ef7f8d215192e73874c225f382c8f7bca896cf6d
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-08-06 22:53:14 +0300

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const createGallery = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.addEventListener("click", onClick);
+galleryContainer.insertAdjacentHTML("beforeend", createGallery);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  const currentGalleryCard = document.querySelector(".gallery__image");
+  if (!currentGalleryCard) {
+    return;
+  }
+
+  const galleryCard = evt.target;
+
+  createImageModal(galleryCard.dataset.source);
+}
+
+function createImageModal(card) {
+  openModal(card);
+
+  document.addEventListener("keydown", closeCard);
+
+  function closeCard(evt) {
+    if (evt.code === "Escape") {
+      const activeModal = document.querySelector(".basicLightbox");
+      activeModal?.remove("basicLightbox--visible");
+      document.removeEventListener("keydown", closeCard);
+    }
+  }
+}
+
+function openModal(card) {
+  const imageModal = basicLightbox.create(`
+    <div class="modal">
+      <img
+        class="gallery__image"
+        src="${card}"
+        data-source="${card}"
+      />
+    </div>
+      `);
+  imageModal.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryListRef = document.querySelector(".gallery");
+const createGallery = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+galleryListRef.addEventListener("click", onClick);
+galleryListRef.insertAdjacentHTML("beforeend", createGallery);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+  </a>
+    </li>`;
+    })
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  const currentGalleryCard = document.querySelector(".gallery__image");
+
+  if (!currentGalleryCard) {
+    return;
+  }
+  createImageModal();
+}
+
+function createImageModal() {
+  createLiteBox();
+}
+
+function createLiteBox() {
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+  return lightbox;
+}
