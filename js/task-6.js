3e783389c466095339e90b112179dbbafe709bc4
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2026-04-27 20:41:20 +0300

    updated task6

--- js/task-6.js
+++ js/task-6.js
@@ -6,7 +6,7 @@ const refs = {
 };
 
 refs.inputValue.addEventListener('input', getInputValue);
-refs.createButton.addEventListener('click', createBoxes);
+refs.createButton.addEventListener('click', handleCreateClick);
 refs.destroyButton.addEventListener('click', destroyBoxes);
 
 function getRandomHexColor() {
@@ -15,17 +15,33 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+function handleCreateClick() {
+  const amount = Number(refs.inputValue.value);
+
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+
+  createBoxes(amount);
+
+  refs.inputValue.value = '';
+}
+
 function createBoxes(amount) {
-  amount = getInputValue();
+  destroyBoxes();
 
   const basicBoxSize = 30;
+  const elements = [];
 
   for (let i = 0; i < amount; i++) {
     const boxSize = basicBoxSize + i * 10;
     const createDivEl = document.createElement('div');
+
     createDivEl.style.cssText = `width: ${boxSize}px; height: ${boxSize}px; background-color: ${getRandomHexColor()}`;
-    refs.boxes.append(createDivEl);
+    elements.push(createDivEl);
   }
+
+  refs.boxes.append(...elements);
 }
 
 function destroyBoxes() {
